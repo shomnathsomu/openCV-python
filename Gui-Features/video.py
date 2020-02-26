183e4eaea91c494bc3478cf0d1f2341c14cbb7c4
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-02-26 10:58:35 +0600

    Saving a video

--- Gui-Features/video.py
+++ Gui-Features/video.py
@@ -1,4 +1,4 @@
-# Capture Video from Camera
+####### Capture Video from Camera ######
 import numpy as np
 import cv2
 
@@ -21,7 +21,7 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-# Playing Video from file
+####### Playing Video from file #######
 import numpy as np
 import cv2
 
@@ -37,4 +37,33 @@ while(cap.isOpened()):
         break
 
 cap.release()
+cv2.destroyAllWindows()
+
+######## Saving a video #######
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
 cv2.destroyAllWindows()
