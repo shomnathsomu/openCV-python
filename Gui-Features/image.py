8437add6c735d5b0e2fa4cbaeca36b17c153dd97
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-02-26 09:51:14 +0600

    Getting Started with Images

---
+++ Gui-Features/image.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+# Read an image
+# Load a color image in grayscale
+img = cv2.imread('messi.jpg',0)
+
+# Display an image
+cv2.imshow('image',img)
+
+# keyboard binding function
+k = cv2.waitKey(0)
+
+# For a 64-bit Machine
+# k = cv2.waitKey(0) & 0xFF
+
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('messigray.png',img) # Write an image
+    cv2.destroyAllWindows()
+    
+# Using Matplotlib
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
