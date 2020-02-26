c6f88b84e4c3a038d3f0dba3e72fe5fde7604576
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-02-26 10:49:18 +0600

    Playing Video from file

--- Gui-Features/video.py
+++ Gui-Features/video.py
@@ -19,3 +19,22 @@ while(True):
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+
+# Playing Video from file
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('vtest.avi')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
