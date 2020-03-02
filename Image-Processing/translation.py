0d2d2f5bde613d330c2062a28a338a5dd2e89e3f
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-02 11:42:14 +0600

    Translation of an image

---
+++ Image-Processing/translation.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('query/query_01.bmp',0)
+rows,cols = img.shape
+
+M = np.float32([[1,0,100],[0,1,50]])
+dst = cv2.warpAffine(img,M,(cols,rows))
+
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
