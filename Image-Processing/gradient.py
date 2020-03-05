e9311eb2b0d4ed4f6f207a1f18b12e6a16aba214
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-03-05 16:53:27 +0600

    Create Morphological Gradient Operation

---
+++ Image-Processing/gradient.py
@@ -0,0 +1,15 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+img = cv2.imread('samples/new_011.jpg',0)
+kernel = np.ones((5,5),np.uint8)
+gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+
+cv2.imwrite('1.png', gradient)
+
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(gradient),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
